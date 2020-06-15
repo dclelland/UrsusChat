05967d16d5f65783d5db4982cb0d43d25401c1de
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-15 21:51:49 +1200

    Generate Ursus Chat project

--- Pods/Target Support Files/Pods-Ursus Chat/Pods-Ursus Chat-umbrella.h
+++ Pods/Target Support Files/Pods-Ursus Chat/Pods-Ursus Chat-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_UrchatVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_UrchatVersionString[];
+FOUNDATION_EXPORT double Pods_Ursus_ChatVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Ursus_ChatVersionString[];
 
