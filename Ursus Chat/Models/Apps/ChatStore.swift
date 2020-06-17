b8fa359eb99bd2fc946fc7de6c2605f4af3b4ac7
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-17 21:11:15 +1200

    Split off structs to mirror what's in chat-store.hoon

---
+++ Ursus Chat/Models/Apps/ChatStore.swift
@@ -0,0 +1,144 @@
+//
+//  ChatStore.swift
+//  Ursus Chat
+//
+//  Created by Daniel Clelland on 17/06/20.
+//  Copyright © 2020 Protonome. All rights reserved.
+//
+
+import Foundation
+import Ursus
+
+class ChatStore: UrsusApp {
+    
+    enum Letter: Decodable {
+        
+        struct Code: Decodable {
+            
+            var expression: String
+            var output: [[String]]
+            
+        }
+        
+        case text(String)
+        case url(URL)
+        case code(Code)
+        case me(String)
+        
+        enum CodingKeys: String, CodingKey {
+            
+            case text
+            case url
+            case code
+            case me
+            
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            switch Set(container.allKeys) {
+            case [.text]:
+                self = .text(try container.decode(String.self, forKey: .text))
+            case [.url]:
+                self = .url(try container.decode(URL.self, forKey: .url))
+            case [.code]:
+                self = .code(try container.decode(Code.self, forKey: .code))
+            case [.me]:
+                self = .me(try container.decode(String.self, forKey: .code))
+            default:
+                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Failed to decode \(type(of: self)); available keys: \(container.allKeys)"))
+            }
+        }
+        
+    }
+    
+    struct Envelope: Decodable {
+
+        var when: Date
+        var author: String
+        var number: Int
+        var letter: Letter
+        var uid: String
+        
+    }
+    
+    struct Config: Decodable {
+        
+        var length: Int
+        var read: Int
+        
+    }
+    
+    struct Mailbox: Decodable {
+        
+        var config: Config
+        var envelopes: [Envelope]
+        
+    }
+    
+    typealias Inbox = [String: Mailbox]
+
+}
+
+extension ChatStore {
+    
+    enum Update: Decodable {
+        
+        struct Create: Decodable {
+            
+            var path: String
+            
+        }
+        
+        struct Delete: Decodable {
+            
+            var path: String
+            
+        }
+        
+        struct Message: Decodable {
+            
+            var path: String
+            var envelope: Envelope
+            
+        }
+        
+        struct Read: Decodable {
+            
+            var path: String
+            
+        }
+        
+        case create(Create)
+        case delete(Delete)
+        case message(Message)
+        case read(Read)
+        
+        enum CodingKeys: String, CodingKey {
+            
+            case create
+            case delete
+            case message
+            case read
+            
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            switch Set(container.allKeys) {
+            case [.create]:
+                self = .create(try container.decode(Create.self, forKey: .create))
+            case [.delete]:
+                self = .delete(try container.decode(Delete.self, forKey: .delete))
+            case [.message]:
+                self = .message(try container.decode(Message.self, forKey: .message))
+            case [.read]:
+                self = .read(try container.decode(Read.self, forKey: .read))
+            default:
+                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Failed to decode \(type(of: self)); available keys: \(container.allKeys)"))
+            }
+        }
+        
+    }
+    
+}
--- Ursus Chat/Models/Apps/ChatView.swift
+++ Ursus Chat/Models/Apps/ChatView.swift
@@ -24,134 +24,14 @@ class ChatView: UrsusApp {
         return subscribeRequest(path: "/primary", handler: handler)
     }
     
+}
+
+extension ChatView {
+    
     enum Primary: Decodable {
         
-        struct ChatInitial: Decodable {
-            
-            struct Config: Decodable {
-                
-                var length: Int
-                var read: Int
-                
-            }
-            
-            struct Envelope: Decodable {
-                
-                enum Letter: Decodable {
-                    
-                    struct Code: Decodable {
-                        
-                        var expression: String
-                        var output: [[String]]
-                        
-                    }
-                    
-                    case text(String)
-                    case url(URL)
-                    case code(Code)
-                    case me(String)
-                    
-                    enum CodingKeys: String, CodingKey {
-                        
-                        case text
-                        case url
-                        case code
-                        case me
-                        
-                    }
-                    
-                    init(from decoder: Decoder) throws {
-                        let container = try decoder.container(keyedBy: CodingKeys.self)
-                        switch Set(container.allKeys) {
-                        case [.text]:
-                            self = .text(try container.decode(String.self, forKey: .text))
-                        case [.url]:
-                            self = .url(try container.decode(URL.self, forKey: .url))
-                        case [.code]:
-                            self = .code(try container.decode(Code.self, forKey: .code))
-                        case [.me]:
-                            self = .me(try container.decode(String.self, forKey: .code))
-                        default:
-                            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Failed to decode \(type(of: self)); available keys: \(container.allKeys)"))
-                        }
-                    }
-                    
-                }
-
-                var when: Date
-                var author: String
-                var number: Int
-                var letter: Letter
-                var uid: String
-                
-            }
-            
-            var config: Config
-            var envelopes: [Envelope]
-            
-        }
-        
-        enum ChatUpdate: Decodable {
-            
-            struct Create: Decodable {
-                
-                var path: String
-                
-            }
-            
-            struct Delete: Decodable {
-                
-                var path: String
-                
-            }
-            
-            struct Message: Decodable {
-                
-                var path: String
-                var envelope: Primary.ChatInitial.Envelope
-                
-            }
-            
-            struct Read: Decodable {
-                
-                var path: String
-                
-            }
-            
-            case create(Create)
-            case delete(Delete)
-            case message(Message)
-            case read(Read)
-            
-            enum CodingKeys: String, CodingKey {
-                
-                case create
-                case delete
-                case message
-                case read
-                
-            }
-            
-            init(from decoder: Decoder) throws {
-                let container = try decoder.container(keyedBy: CodingKeys.self)
-                switch Set(container.allKeys) {
-                case [.create]:
-                    self = .create(try container.decode(Create.self, forKey: .create))
-                case [.delete]:
-                    self = .delete(try container.decode(Delete.self, forKey: .delete))
-                case [.message]:
-                    self = .message(try container.decode(Message.self, forKey: .message))
-                case [.read]:
-                    self = .read(try container.decode(Read.self, forKey: .read))
-                default:
-                    throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Failed to decode \(type(of: self)); available keys: \(container.allKeys)"))
-                }
-            }
-            
-        }
-        
-        case chatInitial([String: ChatInitial])
-        case chatUpdate(ChatUpdate)
+        case chatInitial(ChatStore.Inbox)
+        case chatUpdate(ChatStore.Update)
         
         enum CodingKeys: String, CodingKey {
             
@@ -164,9 +44,9 @@ class ChatView: UrsusApp {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             switch Set(container.allKeys) {
             case [.chatInitial]:
-                self = .chatInitial(try container.decode([String: ChatInitial].self, forKey: .chatInitial))
+                self = .chatInitial(try container.decode(ChatStore.Inbox.self, forKey: .chatInitial))
             case [.chatUpdate]:
-                self = .chatUpdate(try container.decode(ChatUpdate.self, forKey: .chatUpdate))
+                self = .chatUpdate(try container.decode(ChatStore.Update.self, forKey: .chatUpdate))
             default:
                 throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Failed to decode \(type(of: self)); available keys: \(container.allKeys)"))
             }
