d4fb2c7f3cddc681f93030b2519613127492f8e3
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-18 22:32:37 +1200

    Finished S3Store setup

--- Ursus Chat/AppDelegate.swift
+++ Ursus Chat/AppDelegate.swift
@@ -32,15 +32,15 @@ import Ursus
 //            ursus.contactView(ship: ship).primary { event in
 //                print("[contact-view /primary]", event)
 //            }
-            ursus.metadataStore(ship: ship).appName(app: "chat") { event in
-                print("[metadata-store /app-name/chat]", event)
-            }
-            ursus.metadataStore(ship: ship).appName(app: "contacts") { event in
-                print("[metadata-store /app-name/contacts]", event)
-            }
-//            ursus.s3Store(ship: ship).all { event in
-//                print("[s3-store /all]", event.map { String(data: $0, encoding: .utf8)! })
+//            ursus.metadataStore(ship: ship).appName(app: "chat") { event in
+//                print("[metadata-store /app-name/chat]", event)
 //            }
+//            ursus.metadataStore(ship: ship).appName(app: "contacts") { event in
+//                print("[metadata-store /app-name/contacts]", event)
+//            }
+            ursus.s3Store(ship: ship).all { event in
+                print("[s3-store /all]", event)
+            }
         }
         
         return true
--- Ursus Chat/Models/Apps/S3Store.swift
+++ Ursus Chat/Models/Apps/S3Store.swift
@@ -20,10 +20,96 @@ extension Ursus {
 
 class S3Store: UrsusApp {
     
-    #warning("Implement decoder for S3Store.AllResponse")
-    
-    @discardableResult func all(handler: @escaping (SubscribeEvent<Data>) -> Void) -> DataRequest {
+    @discardableResult func all(handler: @escaping (SubscribeEvent<AllResponse>) -> Void) -> DataRequest {
         return subscribeRequest(path: "/all", handler: handler)
+    }
+    
+}
+
+extension S3Store {
+    
+    enum AllResponse: Decodable {
+        
+        case s3Update(S3Update)
+        
+        enum CodingKeys: String, CodingKey {
+            
+            case s3Update
+            
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            switch Set(container.allKeys) {
+            case [.s3Update]:
+                self = .s3Update(try container.decode(S3Update.self, forKey: .s3Update))
+            default:
+                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Failed to decode \(type(of: self)); available keys: \(container.allKeys)"))
+            }
+        }
+        
+    }
+    
+}
+
+extension S3Store {
+    
+    struct Credentials: Decodable {
+        
+//        var endpoint: String
+//        var accessKeyID: String
+//        var secretAccessKey: String
+//
+//        enum CodingKeys: String, CodingKey {
+//
+//            case endpoint = "endpoint"
+//            case accessKeyID = "accessKeyId"
+//            case secretAccessKey = "secretAccessKey"
+//
+//        }
+        
+    }
+    
+    struct Configuration: Decodable {
+        
+//        var buckets: Set<String>
+//        var currentBucket: String
+//
+//        enum CodingKeys: String, CodingKey {
+//
+//            case buckets = "buckets"
+//            case currentBucket = "currentBucket"
+//
+//        }
+        
+    }
+    
+    enum S3Update: Decodable {
+        
+        #warning("Implement decoder for S3Store.S3Update: Several cases remaining; issues with CodingKeys")
+        
+        case credentials(Credentials)
+        case configuration(Configuration)
+        
+        enum CodingKeys: String, CodingKey {
+            
+            case credentials
+            case configuration
+            
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            switch Set(container.allKeys) {
+            case [.credentials]:
+                self = .credentials(try container.decode(Credentials.self, forKey: .credentials))
+            case [.configuration]:
+                self = .configuration(try container.decode(Configuration.self, forKey: .configuration))
+            default:
+                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Failed to decode \(type(of: self)); available keys: \(container.allKeys)"))
+            }
+        }
+        
     }
     
 }
