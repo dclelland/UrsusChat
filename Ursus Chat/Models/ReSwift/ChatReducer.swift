77460ce0dbb0bfe9d2ff42c36f143252422ab216
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-24 16:38:26 +1200

    Set up chatstate reducers

--- Ursus Chat/Models/ReSwift/ChatReducer.swift
+++ Ursus Chat/Models/ReSwift/ChatReducer.swift
@@ -31,17 +31,17 @@ func chatReducer(action: Action, state: ChatState?) -> ChatState {
     
     switch action {
     case SubscribeEvent<ChatView.PrimaryResponse>.update(let response):
-        chatViewReducer(response: response, state: state)
+        chatViewReducer(response: response, state: &state)
     case SubscribeEvent<ChatHook.SyncedResponse>.update(let response):
-        chatHookReducer(response: response, state: state)
+        chatHookReducer(response: response, state: &state)
     case SubscribeEvent<InviteStore.AllResponse>.update(let response):
-        inviteStoreReducer(response: response, state: state)
+        inviteStoreReducer(response: response, state: &state)
     case SubscribeEvent<PermissionStore.AllResponse>.update(let response):
-        permissionStoreReducer(response: response, state: state)
+        permissionStoreReducer(response: response, state: &state)
     case SubscribeEvent<ContactView.PrimaryResponse>.update(let response):
-        contactViewReducer(response: response, state: state)
+        contactViewReducer(response: response, state: &state)
     case SubscribeEvent<MetadataStore.AppNameResponse>.update(let response):
-        metadataStoreReducer(response: response, state: state)
+        metadataStoreReducer(response: response, state: &state)
     default:
         break
     }
@@ -49,26 +49,26 @@ func chatReducer(action: Action, state: ChatState?) -> ChatState {
     return state
 }
 
-func chatViewReducer(response: ChatView.PrimaryResponse, state: ChatState) {
+func chatViewReducer(response: ChatView.PrimaryResponse, state: inout ChatState) {
     
 }
 
-func chatHookReducer(response: ChatHook.SyncedResponse, state: ChatState) {
+func chatHookReducer(response: ChatHook.SyncedResponse, state: inout ChatState) {
     
 }
 
-func inviteStoreReducer(response: InviteStore.AllResponse, state: ChatState) {
+func inviteStoreReducer(response: InviteStore.AllResponse, state: inout ChatState) {
     
 }
 
-func permissionStoreReducer(response: PermissionStore.AllResponse, state: ChatState) {
+func permissionStoreReducer(response: PermissionStore.AllResponse, state: inout ChatState) {
     
 }
 
-func contactViewReducer(response: ContactView.PrimaryResponse, state: ChatState) {
+func contactViewReducer(response: ContactView.PrimaryResponse, state: inout ChatState) {
     
 }
 
-func metadataStoreReducer(response: MetadataStore.AppNameResponse, state: ChatState) {
+func metadataStoreReducer(response: MetadataStore.AppNameResponse, state: inout ChatState) {
     
 }
