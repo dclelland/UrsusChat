4d07b9b3b6453298b277db61ecac043fcf4c12ef
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-25 21:39:19 +1200

    Stash

--- Ursus Chat/Views/AuthenticationView.swift
+++ Ursus Chat/Views/AuthenticationView.swift
@@ -12,13 +12,42 @@ struct AuthenticationView: View {
     
     @State private var url: URL?
     
-    @State private var code: String?
+    @State private var code: String = ""
     
     var body: some View {
-        VStack {
+        VStack() {
             Text("Urbit URL")
-            TextField("URL", value: $url, formatter: URLFormatter())
+            TextField("http://sampel-palnet.arvo.network", value: $url, formatter: URLFormatter())
+            Text("Access Key")
+            Text("Get key from Bridge, or +code in dojo")
+                .font(.caption)
+            SecureField("sampel-ticlyt-migfun-falmel", text: $code) // Monospaced; password
+            Button(action: continueButtonTapped) {
+                Text("Continue")
+            }
+            Button(action: bridgeButtonTapped) {
+                Text("Open Bridge")
+            }
+            Button(action: purchaseButtonTapped) {
+                Text("Purchase an Urbit ID")
+            }
         }
+    }
+    
+}
+
+extension AuthenticationView {
+    
+    private func continueButtonTapped() {
+        
+    }
+    
+    private func bridgeButtonTapped() {
+        
+    }
+    
+    private func purchaseButtonTapped() {
+        
     }
     
 }
