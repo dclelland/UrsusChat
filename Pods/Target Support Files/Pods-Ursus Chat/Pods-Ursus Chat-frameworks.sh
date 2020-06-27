874794ba38950b7a45f019164c1c4e61107c84ff
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-27 16:02:05 +1200

    Move integer boilerplate guff into a protocol

--- Pods/Target Support Files/Pods-Ursus Chat/Pods-Ursus Chat-frameworks.sh
+++ Pods/Target Support Files/Pods-Ursus Chat/Pods-Ursus Chat-frameworks.sh
@@ -162,7 +162,6 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireLogger/AlamofireLogger.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BigInt/BigInt.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KeychainAccess/KeychainAccess.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parity/Parity.framework"
@@ -171,7 +170,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireLogger/AlamofireLogger.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BigInt/BigInt.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KeychainAccess/KeychainAccess.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parity/Parity.framework"
