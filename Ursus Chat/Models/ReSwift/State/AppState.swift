5e5b5d8fee8c0331b0f6b313282e4dbc12bad3eb
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-30 19:57:48 +1200

    Added loginstate

---
+++ Ursus Chat/Models/ReSwift/State/AppState.swift
@@ -0,0 +1,17 @@
+//
+//  AppState.swift
+//  Ursus Chat
+//
+//  Created by Daniel Clelland on 30/06/20.
+//  Copyright © 2020 Protonome. All rights reserved.
+//
+
+import Foundation
+import ReSwift
+
+enum AppState: StateType {
+    
+    case login(LoginState)
+    case chat(ChatState)
+    
+}
---
+++ Ursus Chat/Models/ReSwift/State/LoginState.swift
@@ -0,0 +1,17 @@
+//
+//  LoginState.swift
+//  Ursus Chat
+//
+//  Created by Daniel Clelland on 30/06/20.
+//  Copyright © 2020 Protonome. All rights reserved.
+//
+
+import Foundation
+import ReSwift
+
+struct LoginState: StateType {
+    
+    var url: URL
+    var code: String
+    
+}
