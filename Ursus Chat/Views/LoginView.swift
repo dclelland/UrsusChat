14e2cfda9c0c0424a56e69eb0fcc031c6560347a
Daniel Clelland <daniel.clelland@gmail.com>
2020-07-06 21:45:33 +1200

    Disable the login view while unauthenticating

--- Ursus Chat/Views/LoginView.swift
+++ Ursus Chat/Views/LoginView.swift
@@ -18,6 +18,8 @@ struct LoginView: View {
     
     @State var code: String = ""
     
+    @State var isAuthenticating: Bool = false
+    
     var body: some View {
         NavigationView {
             Form {
@@ -44,6 +46,7 @@ struct LoginView: View {
                     }
                 }
             }
+            .disabled(isAuthenticating)
             .navigationBarTitle("Login")
         }
     }
@@ -57,6 +60,7 @@ extension LoginView {
             return
         }
         
+        isAuthenticating = true
         store.dispatch(sessionThunk(url: url, code: code))
     }
     
