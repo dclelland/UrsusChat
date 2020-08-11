00b8d2e512b972e0e16c2ca7f0b46f6ed75e8ade
Daniel Clelland <daniel.clelland@gmail.com>
2020-08-11 18:47:08 +1200

    Commit navigation link experiment but comment it out

--- Ursus Chat/Views/ChatListView.swift
+++ Ursus Chat/Views/ChatListView.swift
@@ -13,9 +13,29 @@ import UrsusSigil
 
 struct ChatListView: View {
     
+//    struct Destination: View {
+//
+//        var selectedPath: UrsusAirlock.Path? = nil
+//
+//        var body: some View {
+//            if let path = selectedPath {
+//                return AnyView(
+//                    ChatView(path: path)
+//                )
+//            } else {
+//                return AnyView(
+//                    EmptyView()
+//                )
+//            }
+//        }
+//
+//    }
+    
     @EnvironmentObject var store: AppStore
     
     @State private var selectedPath: UrsusAirlock.Path? = nil
+    
+//    @State private var selectedPathIsActive: Bool = false
     
     @State private var selectedNewChatSheetViewMode: NewChatSheetView.Mode? = nil
     
@@ -25,14 +45,31 @@ struct ChatListView: View {
     
     var ship: Ship
     
+    #warning("TODO: Remove the clumsy ZStack hack in iOS 14 (https://stackoverflow.com/questions/61005551)")
+    
     var body: some View {
         NavigationView {
+//            ZStack {
+//            NavigationLink(destination: Destination(selectedPath: selectedPath), isActive: $selectedPathIsActive) {
+//                EmptyView()
+//            }
             List {
                 ConnectionView()
                 ForEach(store.state.subscription.inbox.sorted(by: \.value.when).reversed(), id: \.key) { (path, mailbox) in
                     NavigationLink(destination: ChatView(path: path), tag: path, selection: self.$selectedPath) {
                         ChatListRow(path: path)
                     }
+//                    Button(
+//                        action: {
+//                            self.selectedPath = path
+//                            self.selectedPathIsActive = true
+//                        },
+//                        label: {
+//                            NavigationLink(destination: EmptyView()) {
+//                                ChatListRow(path: path)
+//                            }
+//                        }
+//                    )
                 }
             }
             .navigationBarTitle("Chats")
@@ -72,6 +109,7 @@ struct ChatListView: View {
             .introspectTableView { tableView in
                 tableView.tableFooterView = UIView()
             }
+//            }
         }
         .sheet(item: $selectedNewChatSheetViewMode) { mode in
             NewChatSheetView(mode: mode)
